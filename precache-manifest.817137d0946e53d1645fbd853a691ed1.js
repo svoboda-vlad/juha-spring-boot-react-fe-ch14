83f942240d0aad3a5616d6bb475d7d3a37ebc17c
Vladimir Svoboda <svoboda-vlad@seznam.cz>
2020-09-25 10:42:06 +0000

    Updates

---
+++ precache-manifest.817137d0946e53d1645fbd853a691ed1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d3280ca093dbde33b1ca046575ed942b",
+    "url": "/juha-spring-boot-react-fe-ch14/index.html"
+  },
+  {
+    "revision": "c9e4f4952731c3b18701",
+    "url": "/juha-spring-boot-react-fe-ch14/static/css/2.2e7cf2a9.chunk.css"
+  },
+  {
+    "revision": "a66b79065c5bd1d2fca0",
+    "url": "/juha-spring-boot-react-fe-ch14/static/css/main.692d75bb.chunk.css"
+  },
+  {
+    "revision": "c9e4f4952731c3b18701",
+    "url": "/juha-spring-boot-react-fe-ch14/static/js/2.a576f6df.chunk.js"
+  },
+  {
+    "revision": "a66b79065c5bd1d2fca0",
+    "url": "/juha-spring-boot-react-fe-ch14/static/js/main.53bd54a5.chunk.js"
+  },
+  {
+    "revision": "db23ad1cabc7c8d2a4fa",
+    "url": "/juha-spring-boot-react-fe-ch14/static/js/runtime~main.03a27c13.js"
+  }
+]);
